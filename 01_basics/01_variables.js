5e4a0bde5698f5120200a34dd6c343979275bf52
Abhishekyadavdev <143774373+Abhishekyadavdev@users.noreply.github.com>
2023-09-15 13:23:11 +0000

    contai ner config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountid="1223";
+let acccountemail='ay340608@gmail.com';
+var accountpassword='1234';
+accountcity='mumbai';
+/*prefer not to use var
+beacuse of issuein block scope and functional scope
+*/ 
+// accountid=2;
+acccountemail='mr.abhishekyadav48@gamil.com';
+accountpassword="22222222"
+console.log(accountid);
+console.table([accountid,acccountemail,accountpassword,accountcity]); 
