e2d345e966828826d3d37f7623ab903f93636825
Abhishekyadavdev <143774373+Abhishekyadavdev@users.noreply.github.com>
2023-09-15 14:37:50 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+ "use strict";//treat all js code as newr version
+// alert("hello");//we are using nodejs not browser
+console.log(3+3);
+console.log("abhishek");
+let name="abhishek";
+let age=17;
+let islogin=false;
+// number=>2to power 53
+// bigint
+// String=>""
+// boolean=>true,false
+// null=>standalone value
+// undefined=>value is not defined
+// symbol=>unique
+// object
+console.log(typeof age);
