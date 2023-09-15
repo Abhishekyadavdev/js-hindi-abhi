749c697f3fc90d43e7cfb83e349eb9f2a9b8a52d
Abhishekyadavdev <143774373+Abhishekyadavdev@users.noreply.github.com>
2023-09-15 15:02:52 +0000

    conversion datatypes

---
+++ 01_basics/03_datatype_conversion.js
@@ -0,0 +1,15 @@
+let Score="33";
+console.log(typeof(Score));
+let valueInNumber=Number(Score);
+console.log(typeof(valueInNumber));
+// "33abc"=nan
+// undefined=>nan
+// null=>nan
+let isloggedIn=1;
+let booleanIsloggedIn=Boolean(isloggedIn)
+console.log(booleanIsloggedIn);
+// ""=>false
+let number=33;
+let Stringnumber=String(number);
+console.log(Stringnumber);
+console.log(typeof Stringnumber);
