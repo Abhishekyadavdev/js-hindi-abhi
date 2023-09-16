798446b92aef0f0ced079a7d3655713cb2953a8d
ABHISHEK YADAV <143774373+Abhishekyadavdev@users.noreply.github.com>
2023-09-16 13:50:44 +0000

    String

---
+++ String/01_String.js
@@ -0,0 +1,24 @@
+const name="abhi"
+const repocount=50
+// console.log(name+repocount +""); // ' `````````````
+
+console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+const gamename=new String('abhishek')
+console.log(gamename[0]);
+console.log(gamename._proto_);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.toLowerCase());
+console.log(gamename.charAt(6));
+console.log(gamename.indexOf('k'));
+const newString =gamename.substring(0,4)
+console.log(newString);
+const anotherString=gamename.slice(-8,4)
+console.log(anotherString);
+const newStringone="  abhishek  "
+console.log(newStringone);
+console.log(newStringone.trim());
+const url ="https//:hitesh);.com/hitesh%20choudhary";
+console.log(url.replace('%20','-'))
+console.log(url.includes('hitesh'))
+
