f43b8576eec85fb342322fa69dd3166d0f8a515e
ABHISHEK YADAV <143774373+Abhishekyadavdev@users.noreply.github.com>
2023-09-16 11:26:49 +0000

    datatype conversion

--- 01_basics/03_datatype_conversion.js
+++ 01_basics/03_datatype_conversion.js
@@ -13,3 +13,54 @@ let number=33;
 let Stringnumber=String(number);
 console.log(Stringnumber);
 console.log(typeof Stringnumber);
+
+// **********operation*************
+let value=3;
+let negvalue=-value;
+console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+let str1="hello";
+let str2="abhi";
+let str3=str1+str2;
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log(+true);
+let num1, num2,num3
+num1=num2=num3=2+2
+let gamecounter=100;
+gamecounter++;
+console.log(gamecounter);
+
+++gamecounter;
+console.log(gamecounter);
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+console.log("2">1);
+console.log("02">1);
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+// ===
+console.log("2"===2);
+
+
+
+
+
+
+
+
