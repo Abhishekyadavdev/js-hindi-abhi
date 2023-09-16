35658e4a82157d966b76fd624eb266c68f2d9a14
ABHISHEK YADAV <143774373+Abhishekyadavdev@users.noreply.github.com>
2023-09-16 12:09:17 +0000

    datatype_interviewquestion

---
+++ 01_basics/datatype_interviewquestion.js
@@ -0,0 +1,43 @@
+// primitive datatype
+
+// 7 types: String,number,boolean,null,undefined,Symbol,BigInt
+// non-primitive
+// array ,objects,function,
+// which type of language javascrift
+// dynamicaly languge data type automatically assigned at the time
+const score=100;
+const scorevalue=100.3;
+const isloggedIn= false;
+const outsideTemp= null;
+let userEmail;
+const id =Symbol('123')
+const anotherid= Symbol('123')
+console.log(id===anotherid);
+ const bignumber=12345678944562
+const heros=["saktimAN","hero"];
+
+    let myobj={
+    name: "abhishek yadav",
+    age:22
+}
+ const myfunction =function(){
+    console.log("hello world");
+
+}
+console.log(typeof bignumber);
+console.log(typeof myfunction);
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//     //    Object  =>  object
