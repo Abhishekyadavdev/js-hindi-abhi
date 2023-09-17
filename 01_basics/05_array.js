7eddfb95121fdcbbf6b4327080efbb382ebf7d23
ABHISHEK YADAV <143774373+Abhishekyadavdev@users.noreply.github.com>
2023-09-17 11:59:02 +0000

    array

---
+++ 01_basics/05_array.js
@@ -0,0 +1,36 @@
+// array
+// const myarr=[0,1,2,3,4,5]
+// const name=["ad","asdf"]
+// const myarr2=new Array(1,2,3,4,5)
+// console.log(myarr[0]);
+// // array method
+// myarr.push(6)
+// myarr.push(7)
+// console.log(myarr);
+// myarr.pop()
+// console.log(myarr);
+// myarr.unshift(8)
+// // myarr.shift()
+// console.log(myarr.includes(9));
+// console.log(myarr.indexOf(9));
+// console.log(myarr);
+// const newarr=myarr.join()
+// console.log(newarr);
+
+
+
+
+
+// slice ,splice
+// console.log(("a",myarr));
+// const myn1=myarr.slice(1,3)
+// console.log(myn1);
+// console.log(("b",myarr));
+
+const nam =['a','b','b']
+const name=['av','an','ak']
+// nam.push(name)
+// console.log(nam);
+
+ const all =nam.concat(name)
+console.log(all);
