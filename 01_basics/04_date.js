387cc329b8c682456c8cd11c52b59d9ffce78b41
ABHISHEK YADAV <143774373+Abhishekyadavdev@users.noreply.github.com>
2023-09-17 10:43:31 +0000

    dates

---
+++ 01_basics/04_date.js
@@ -0,0 +1,28 @@
+// Date
+// let mydate=new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleDateString());
+// console.log(typeof mydate);
+// let mycreatedate=new Date(2023,0,23, 5,3)
+let mycreatedate=new Date("2023-09-17")
+
+// console.log(mycreatedate.toLocaleDateString());
+
+let myTimeStam=Date.now()
+// console.log(myTimeStam);
+// console.log(mycreatedate.getTime());
+
+let newDate=new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+// ${newDate.getDay()}
+newDate.toLocaleDateString('default',{
+    weekday:"long",
+    // timeZone:
+})
+
+
+
+
+
