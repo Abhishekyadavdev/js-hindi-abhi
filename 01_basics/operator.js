6ea175d5d652f07fcff2220ee8a52a6c3607635c
ABHISHEK YADAV <143774373+Abhishekyadavdev@users.noreply.github.com>
2023-09-20 10:09:39 +0000

    Stringmethod

---
+++ 01_basics/operator.js
@@ -0,0 +1,14 @@
+// **************************operator**************
+//************** arithmetic operator*****************
+let a = 3;
+let b=5;
+let x = (100 + 50) * a;
+let c=a/b;
+let d =a%b;
+let e=a-b;
+console.table([x,c,d,e]);
+// ***********assignment**********
+let k=10;
+k+=5;
+k-=2;
+console.log(k);
---
+++ String/Stringmethod.js
@@ -0,0 +1,21 @@
+// let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let length = text.length;
+// console.log(length);
+let text = "Apple, Banana, Kiwi";
+let part = text.slice(7, 13);
+console.log(part);
+let str="javascrift"
+let str2="by abhishek"
+let str3=str.concat(str2);
+console.log(str.charAt(2));
+console.log(str.indexOf("t"));
+console.log(str.lastIndexOf("java"));
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str3.slice(3,4));
+console.log(str3);
+let s1="          abhishek yadav";
+console.log(s1.trim());
+console.log(s1.trimStart());
+console.log(s1.trimEnd());
+console.log(str.split(" "));
