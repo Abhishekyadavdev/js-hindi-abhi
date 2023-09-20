e3cec0bb05de48bafaa7375ad303b6c17b8246b5
ABHISHEK YADAV <143774373+Abhishekyadavdev@users.noreply.github.com>
2023-09-20 10:30:06 +0000

    date and time

--- 01_basics/04_date.js
+++ 01_basics/04_date.js
@@ -5,23 +5,33 @@
 // console.log(mydate.toLocaleDateString());
 // console.log(typeof mydate);
 // let mycreatedate=new Date(2023,0,23, 5,3)
-let mycreatedate=new Date("2023-09-17")
+// let mycreatedate=new Date("2023-09-17")
 
-// console.log(mycreatedate.toLocaleDateString());
+// // console.log(mycreatedate.toLocaleDateString());
 
-let myTimeStam=Date.now()
-// console.log(myTimeStam);
-// console.log(mycreatedate.getTime());
+// let myTimeStam=Date.now()
+// // console.log(myTimeStam);
+// // console.log(mycreatedate.getTime());
 
-let newDate=new Date()
-console.log(newDate);
-console.log(newDate.getMonth()+1);
-// ${newDate.getDay()}
-newDate.toLocaleDateString('default',{
-    weekday:"long",
-    // timeZone:
-})
-
+// let newDate=new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// // ${newDate.getDay()}
+// newDate.toLocaleDateString('default',{
+//     weekday:"long",
+//     // timeZone:
+// })
+let today=new Date();
+// console.log(today);
+let date=new Date();
+let Day=date.getDate();
+let Month=date.getMonth()+1;
+let year=date.getFullYear();
+console.log("date is:"+Day+"/"+Month+"/"+year);
+let h=today.getHours();
+let m=today.getMinutes();
+let s=today.getSeconds();
+console.log(h+":"+m+":"+s);
 
 
 
