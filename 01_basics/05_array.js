b43cab4f2bd17e475e0cbd324973cbef3cb5ae2d
ABHISHEK YADAV <143774373+Abhishekyadavdev@users.noreply.github.com>
2023-09-20 17:08:07 +0000

    array

--- 01_basics/05_array.js
+++ 01_basics/05_array.js
@@ -16,8 +16,8 @@
 // console.log(myarr);
 // const newarr=myarr.join("*")
 // console.log(newarr);
-const fruits = ["Banana", "Orange", "Apple", "Kiwi"];
-console.log(fruits.sort());
+// const fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+// console.log(fruits.sort());
 
 
 
@@ -28,10 +28,30 @@ console.log(fruits.sort());
 // console.log(myn1);
 // console.log(("b",myarr));
 
-const nam =['a','b','b']
-const name=['av','an','ak']
+// const nam =['a','b','b']
+// const name=['av','an','ak']
 // nam.push(name)
 // console.log(nam);
 
- const all =nam.concat(name)
+//  const all =nam.concat(name)
+// console.log(all);
+
+const marvles_heros=["thor","ironman","spiderman"]
+const dc=["superman","flash","batman"]
+// marvles_heros.push(dc)
+// console.log(marvles_heros);
+// console.log(marvles_heros[3][1]);
+  const all= marvles_heros.concat(dc)
 console.log(all);
+const hero=[...marvles_heros, ...dc]
+console.log(hero);
+const another=[1,2,3[4,5,6],7,[6,7[4,5]]]
+const real_another=another.flat(5)
+console.log(real_another);
+console.log(Array.isArray("abhishek"));
+console.log(Array.from("abhishek"));
+console.log(Array.from({name:"abhishek"}));
+let score=100
+let score1=200;
+let score3=300;
+console.log(Array.of(score,score1,score3));
