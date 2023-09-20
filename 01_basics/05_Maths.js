f64dc64e8e9dd37cfb0f82c2fe687ae348ebe5f0
ABHISHEK YADAV <143774373+Abhishekyadavdev@users.noreply.github.com>
2023-09-20 11:31:06 +0000

    05_maths

---
+++ 01_basics/05_Maths.js
@@ -0,0 +1,24 @@
+// let a=17;
+// console.log(Math.sqrt(a));
+// console.log(Math.random());
+// console.log(Math.pow(2,2));
+// console.log(Math.floor(4.6));
+// console.log(Math.ceil(4.7));
+// console.log(Math.round(4.3));
+// console.log(Math.abs(-4));
+// console.log(Math.acos(1));
+// console.log(Math.cbrt(6));
+// ***************NUMBer*********
+var x=0; 
+console.log(Number.isFinite(x));
+const score=40;
+console.log(score);
+console.log(typeof(score));
+const balance=new Number(100)
+console.log(balance);
+console.log(balance.toFixed(2));
+console.log(balance.toString().length);
+const b=123.4556;
+console.log(b.toPrecision(3));
+const c=100000000;
+console.log(c.toLocaleString('en-IN'));
