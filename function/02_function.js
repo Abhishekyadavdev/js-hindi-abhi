c604e85c7b028f91696458536a4528480a952eef
ABHISHEK YADAV <143774373+Abhishekyadavdev@users.noreply.github.com>
2023-09-21 13:39:53 +0000

    02_function

---
+++ function/02_function.js
@@ -0,0 +1,17 @@
+function calculatecartprice(valu1 ,value2 ,...num1){
+return num1
+}
+// console.log(calculatecartprice(200,400,50,2000));
+const user ={
+    username: "abhishek",
+    Price:199
+}
+// function handleobject(anyobject){
+//     console.log(`user is ${anyobject.username} and price is ${anyobject.Price}`);
+// }
+// handleobject(user)
+const mynewarry=[200,400,100,600]
+function returnsecondvalue(getarray){
+return getarray[2]
+}
+console.log(returnsecondvalue(mynewarry));
