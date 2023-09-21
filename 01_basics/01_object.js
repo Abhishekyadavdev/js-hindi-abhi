6e4b168265013e9053665c8c7606b74784bb2f11
ABHISHEK YADAV <143774373+Abhishekyadavdev@users.noreply.github.com>
2023-09-21 11:43:47 +0000

    01_objects

---
+++ 01_basics/01_object.js
@@ -0,0 +1,39 @@
+const tinderuser = new Object()
+ tinderuser.id ="123abc"
+ tinderuser.name="sammy"
+ tinderuser.isloggedin =false
+// console.log(tinderuser);
+
+const regularuser ={
+    email: "asdf@gamil.com",
+    fullname:{
+        userfullname:{
+firstname: 'abhishek',
+lastname: "yadav"
+        }
+    }
+}
+console.log(regularuser.fullname.userfullname);
+const obj1={1:"a", 2:"b"};
+const obj2={ 3: "c",4:"d"}
+//  const obej3=Object.assign({},obj1 ,obj2)
+// const obj3={ obj1 ,obj2}
+const obej3={...obj1,...obj2}
+// console.log(obej3);
+const user=[
+    {
+        id:1,
+        email:"ay340608@gmail.com"
+    },
+    {
+
+    },
+    {
+
+    }
+]
+user[1].email
+console.log(tinderuser);
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(tinderuser.hasOwnProperty('isloggedin'));
