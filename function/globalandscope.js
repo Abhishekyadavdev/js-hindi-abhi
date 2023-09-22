0422af5712f3ea177c45216d0c0ff4f376a32d4a
ABHISHEK YADAV <143774373+Abhishekyadavdev@users.noreply.github.com>
2023-09-22 03:50:29 +0000

    globalandscope

---
+++ function/globalandscope.js
@@ -0,0 +1,29 @@
+// var c=300;
+// let a =300;
+//  if(true){
+//     let a=10
+//     const b=20;
+//     // var c=30;
+//     console.log("inner:",a);
+//  }
+//  console.log(a);
+//  console.log(b);
+//  console.log(c);
+function one(){
+    const username="abhhishek"
+    function two(){
+// const website="abhi"
+console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+if(true){
+    const username ="abhishek"
+    if(username==="abhishek"){
+        const website ="youtube"
+console.log(username+website);
+    }
+    // console.log(website);
+}
